89a15cd59ae5668c1154d0fc9043f2a7a0a4934f
Alexandre Visayze <alexandre.visayze.tic@gmail.com>
2017-12-10 20:05:50 +0100

    Etape 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,30 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
- 
-import { AppComponent }  from './app.component';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+
+import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
- 
+import { HeroesComponent }     from './heroes.component';
+import { DashboardComponent }  from './dashboard.component';
+import { HeroService }         from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
+
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+
+export class AppModule {
+}
